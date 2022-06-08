8c0d7010307a6a26eb4bb32014962b399acdb30a
Alex Ferrari <a.ferrari88@gmail.com>
2022-06-08 12:26:38 +0200

    Refactor to remove ActionHandler

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
-﻿import { chromium, Page } from "playwright";
-import { ActionHandler } from "./ActionHandler";
-import { ActionObj, ScrapingStrategy } from "./types";
+﻿import { chromium } from "playwright";
+import { Action, ScrapingStrategy } from "./types";
 import { USER_AGENTS } from "./utils/browser-config";
 
 export class Scraper<T> {
   url: string;
   strategy: ScrapingStrategy<T>;
-  actions: ActionObj[] | undefined;
+  actions: Action[] | undefined;
 
-  constructor(
-    url: string,
-    strategy: ScrapingStrategy<T>,
-    actions?: ActionObj[]
-  ) {
+  constructor(url: string, strategy: ScrapingStrategy<T>, actions?: Action[]) {
     this.url = url;
     this.strategy = strategy;
     this.actions = actions;
@@ -23,17 +18,11 @@ export class Scraper<T> {
     const context = await browser.newContext({
       userAgent: USER_AGENTS[Math.floor(Math.random() * USER_AGENTS.length)], // Picks a random user agent
     });
-    const page: Page = await context.newPage();
+    const page = await context.newPage();
     await page.goto(this.url);
     if (this.actions) {
       for (let action of this.actions) {
-        const actionHandler = new ActionHandler(
-          action.type,
-          action.options,
-          page,
-          context
-        );
-        actionHandler.execute();
+        await action.execute(page, context);
       }
     }
     const result = await this.strategy.execute(page);
