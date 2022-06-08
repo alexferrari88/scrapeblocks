4e20ca269cae256fb642060e43f9647a99c0b2b3
Alex Ferrari <a.ferrari88@gmail.com>
2022-06-08 13:09:18 +0200

    Implement Wait action

---
+++ src/scraping-actions/Wait.ts
@@ -0,0 +1,25 @@
+﻿import { BrowserContext, Page } from "playwright";
+import { Action, ActionOptions } from "../types";
+import { delay, validateOptions } from "../utils/utils";
+
+type WaitOptions = {
+  [P in keyof ActionOptions as Exclude<
+    P,
+    "element" | "cookie"
+  >]-?: ActionOptions[P];
+};
+export class Wait implements Action {
+  options: WaitOptions;
+
+  constructor(options: WaitOptions) {
+    validateOptions(options, {
+      value: "Wait action requires a value (in ms) to wait",
+    });
+    this.options = options;
+  }
+
+  async execute(_page: Page, _context: BrowserContext): Promise<void> {
+    await delay(parseInt(this.options.value));
+    await delay(100);
+  }
+}
