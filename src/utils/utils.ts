cf5d758f309d59eaa54f3b18435101226d724b2f
Alex Ferrari <a.ferrari88@gmail.com>
2022-06-08 12:26:20 +0200

    Add validateOptions func

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,18 @@
 ﻿export function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export type ObjectWithKeysFromGeneric<T, U> = {
+  [key in keyof T]: U;
+};
+
+export function validateOptions<T>(
+  options: T,
+  errors: ObjectWithKeysFromGeneric<T, string>
+) {
+  for (const key in errors) {
+    if (options[key] == undefined) {
+      throw new Error(errors[key]);
+    }
+  }
+}
