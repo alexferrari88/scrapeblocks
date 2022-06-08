6b0f35ca2bcddd7ddeeb486b1dfd14cbec6e923b
Alex Ferrari <a.ferrari88@gmail.com>
2022-06-08 12:35:12 +0200

    Implement Scroll action

--- src/scraping-actions/Scroll.ts
+++ src/scraping-actions/Scroll.ts
@@ -1,1 +1,16 @@
-﻿
+﻿import { BrowserContext, Page } from "playwright";
+import { Action, ActionOptions } from "../types";
+import { delay } from "../utils/utils";
+
+export class Scroll implements Action {
+  options: ActionOptions;
+
+  constructor(options: ActionOptions) {
+    this.options = options;
+  }
+
+  async execute(page: Page, _context?: BrowserContext): Promise<void> {
+    await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+    await delay(100);
+  }
+}
