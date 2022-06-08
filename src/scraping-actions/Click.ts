01914eaa55313f11ac85ff166b8777613df63890
Alex Ferrari <a.ferrari88@gmail.com>
2022-06-08 12:26:45 +0200

    Implement Click action

--- src/scraping-actions/Click.ts
+++ src/scraping-actions/Click.ts
@@ -1,1 +1,28 @@
-﻿
+﻿import { BrowserContext, Page } from "playwright";
+import { Action, ActionOptions } from "../types";
+import { delay, validateOptions } from "../utils/utils";
+
+type ClickOptions = {
+  [P in keyof ActionOptions as Exclude<
+    P,
+    "cookie" | "value"
+  >]-?: ActionOptions[P];
+};
+export class Click implements Action {
+  options: ClickOptions;
+
+  constructor(options: ClickOptions) {
+    validateOptions(options, {
+      element: "Click action requires an element to click",
+    });
+    this.options = options;
+  }
+
+  async execute(page: Page, _context?: BrowserContext): Promise<void> {
+    if (this.options.element == undefined)
+      throw new Error("Click action requires an element to click");
+
+    await page.click(this.options.element);
+    await delay(100);
+  }
+}
