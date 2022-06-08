4fe3a5697a9bff75b287878d06c6bc98847581db
Alex Ferrari <a.ferrari88@gmail.com>
2022-06-08 12:30:48 +0200

    Implement Type action

--- src/scraping-actions/Type.ts
+++ src/scraping-actions/Type.ts
@@ -1,1 +1,23 @@
-﻿
+﻿import { BrowserContext, Page } from "playwright";
+import { Action, ActionOptions } from "../types";
+import { delay, validateOptions } from "../utils/utils";
+
+type TypeOptions = {
+  [P in keyof ActionOptions as Exclude<P, "cookie">]-?: ActionOptions[P];
+};
+export class Type implements Action {
+  options: TypeOptions;
+
+  constructor(options: TypeOptions) {
+    validateOptions(options, {
+      element: "Type action requires an element to type in",
+      value: "Type action requires a value to type",
+    });
+    this.options = options;
+  }
+
+  async execute(page: Page, _context?: BrowserContext): Promise<void> {
+    await page.type(this.options.element, this.options.value);
+    await delay(100);
+  }
+}
