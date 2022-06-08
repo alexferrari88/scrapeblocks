869a7c8be9c7ca72a248df380796d4fa9401df51
Alex Ferrari <a.ferrari88@gmail.com>
2022-06-08 14:14:15 +0200

    Add possibility to run scraper with existing browser, context, page

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-﻿import { chromium } from "playwright";
+﻿import { Browser, BrowserContext, chromium, Page } from "playwright";
 import { Action, ScrapingStrategy } from "./types";
 import { USER_AGENTS } from "./utils/browser-config";
 
 export class Scraper<T> {
+  browser: Browser | undefined;
+  context: BrowserContext | undefined;
+  page: Page | undefined;
   url: string;
   strategy: ScrapingStrategy<T>;
   actions: Action[] | undefined;
@@ -13,20 +16,38 @@ export class Scraper<T> {
     this.actions = actions;
   }
 
+  withBrowser(browser: Browser): Scraper<T> {
+    this.browser = browser;
+    return this;
+  }
+
+  withContext(context: BrowserContext): Scraper<T> {
+    this.context = context;
+    return this;
+  }
+
+  withPage(page: Page): Scraper<T> {
+    this.page = page;
+    return this;
+  }
+
   async run(): Promise<T> {
-    const browser = await chromium.launch();
-    const context = await browser.newContext({
-      userAgent: USER_AGENTS[Math.floor(Math.random() * USER_AGENTS.length)], // Picks a random user agent
-    });
-    const page = await context.newPage();
-    await page.goto(this.url);
+    if (!this.browser && !this.context && !this.page)
+      this.browser = await chromium.launch();
+    if (!this.context && !this.page)
+      this.context = await (this.browser as Browser).newContext({
+        userAgent: USER_AGENTS[Math.floor(Math.random() * USER_AGENTS.length)], // Picks a random user agent
+      });
+    if (!this.page)
+      this.page = await (this.context as BrowserContext).newPage();
+    if (this.url) await this.page.goto(this.url);
     if (this.actions) {
       for (let action of this.actions) {
-        await action.execute(page, context);
+        await action.execute(this.page, this.context);
       }
     }
-    const result = await this.strategy.execute(page);
-    await browser.close();
+    const result = await this.strategy.execute(this.page);
+    if (this.browser) await this.browser.close();
     return result;
   }
 }
