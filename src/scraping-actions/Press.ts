2b226d8bf45b558f7a3cfcabe1b71b3912dd685e
Alex Ferrari <alex.ferrari@nn.nl>
2022-06-09 16:12:34 +0200

    Implement Press action

---
+++ src/scraping-actions/Press.ts
@@ -0,0 +1,23 @@
+﻿import { BrowserContext, Page } from "playwright";
+import { Action, ActionOptions } from "../types";
+import { delay, validateOptions } from "../utils/utils";
+
+type PressOptions = {
+  [P in keyof ActionOptions as Exclude<P, "cookies">]-?: ActionOptions[P];
+};
+export class Press implements Action {
+  options: PressOptions;
+
+  constructor(options: PressOptions) {
+    validateOptions(options, {
+      element: "Press action requires an element to press the key in",
+      value: "Press action requires a value to press",
+    });
+    this.options = options;
+  }
+
+  async execute(page: Page, _context?: BrowserContext): Promise<void> {
+    await page.press(this.options.element, this.options.value);
+    await delay(100);
+  }
+}
