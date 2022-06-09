067474cd5d496602b5401c0ecf15cb7d529582e4
Alex Ferrari <alex.ferrari@nn.nl>
2022-06-09 16:12:48 +0200

    Add index for actions directory

---
+++ src/scraping-actions/index.ts
@@ -0,0 +1,9 @@
+﻿import { AddCookies } from "./AddCookies";
+import { Click } from "./Click";
+import { Press } from "./Press";
+import { Scroll } from "./Scroll";
+import { Select } from "./Select";
+import { Type } from "./Type";
+import { Wait } from "./Wait";
+
+export { AddCookies, Click, Press, Type, Scroll, Select, Wait };
